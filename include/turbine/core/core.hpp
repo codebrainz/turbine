960305229ef0c775079fd03596583a40c7644a21
Matthew Brush <mbrush@codebrainz.ca>
2023-12-19 08:29:11 -0800

    Remove core directory

--- include/turbine/core/core.hpp
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#include <turbine/core/blade.hpp>
-#include <turbine/core/rotor.hpp>
--- include/turbine/turbine.hpp
+++ include/turbine/turbine.hpp
@@ -4,7 +4,6 @@
 #undef unix
 
 #include <turbine/common/common.hpp>
-#include <turbine/core/core.hpp>
 #include <turbine/io/io.hpp>
 #include <turbine/linux/linux.hpp>
 #include <turbine/net/net.hpp>
